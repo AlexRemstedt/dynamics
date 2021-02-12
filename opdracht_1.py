626719baea8a92b844ecd8a2976239578ca8731b
AlexRemstedt <49374085+AlexRemstedt@users.noreply.github.com>
2021-02-12 15:49:51 +0100

    def numeriek made

--- opdracht_1.py
+++ opdracht_1.py
@@ -8,11 +8,37 @@ import numpy as np
 # Variables
 dt = 0.1  # [s] grootte van tijdstappen
 t0 = 0  # [s] Starttijd van simulatie
-t1 = 0.4  # [s] Eindtijd van sim
+t1 = 5.4  # [s] Eindtijd van sim
 x0 = 1.3  # [m] beginpositie
 v0 = 5.5  # [m/s] beginsnelheid
 
 time = np.linspace(t0, t1, 1 + round((t1-t0)/dt))
+
+
+def numeriek(start_positie, start_snelheid, start_acceleratie, tijd):
+	"""
+
+	:param start_positie:
+	:param start_snelheid:
+	:param start_acceleratie:
+	:param tijd:
+	:return:
+	"""
+	# Function variables
+	positie = np.zeros(len(tijd))
+	snelheid = np.zeros(len(tijd))
+	acceleratie = np.zeros(len(tijd))
+
+	positie[0] = start_positie
+	snelheid[0] = start_snelheid
+	acceleratie += start_acceleratie
+
+	for t in range(len(tijd) - 1):
+		dt = tijd[t + 1] - tijd[t]
+		snelheid[t + 1] = snelheid[t] + acceleratie[t] * dt
+		positie[t + 1] = positie[t] + snelheid[t] * dt
+
+	return f'positie: {positie[-1]} \n snelheid: {snelheid[-1]} \n acceleratie: {acceleratie[-1]}'
 
 
 def analytisch(x, v, a, t):
@@ -27,5 +53,3 @@ def analytisch(x, v, a, t):
 	position = x + v * t
 	return position
 
-
-print(analytisch(x0, v0, 0, 0.4))
