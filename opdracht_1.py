f3d864e6f940609b5cc246f95b0c459f72e6dd49
AlexRemstedt <49374085+AlexRemstedt@users.noreply.github.com>
2021-02-12 15:03:15 +0100

    analytisch() made

--- opdracht_1.py
+++ opdracht_1.py
@@ -8,20 +8,24 @@ import numpy as np
 # Variables
 dt = 0.1  # [s] grootte van tijdstappen
 t0 = 0  # [s] Starttijd van simulatie
-t1 = 10  # [s] Eindtijd van sim
+t1 = 0.4  # [s] Eindtijd van sim
 x0 = 1.3  # [m] beginpositie
 v0 = 5.5  # [m/s] beginsnelheid
 
-x = np.linspace(t0, t1, 1 + round((t1-t0)/dt))
+time = np.linspace(t0, t1, 1 + round((t1-t0)/dt))
 
 
 def analytisch(x, v, a, t):
 	"""
 
-	:param x:
-	:param v:
-	:param a:
-	:param t:
+	:param x: beginpositie
+	:param v: beginsnelheid
+	:param a: beginversnelling
+	:param t: tijd
 	:return:
 	"""
-	pass
+	position = x + v * t
+	return position
+
+
+print(analytisch(x0, v0, 0, 0.4))
