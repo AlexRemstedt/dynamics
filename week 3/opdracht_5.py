e5cc00f1a511d392d13db803bbe7576e391dbe59
AlexRemstedt <49374085+AlexRemstedt@users.noreply.github.com>
2021-03-01 20:18:44 +0100

    add vars

--- week 3/opdracht_5.py
+++ week 3/opdracht_5.py
@@ -10,3 +10,14 @@ https://secure.ans-delft.nl/universities/1/courses/63457/assignments/200359/quiz
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Vars
+m = 15          # kg
+k = 90          # N/m
+g = 9.81        # m/s^2
+dt = 0.001      # s
+t0 = 0          # s
+t1 = 10         # s
+y0 = 0.7        # m
+v0 = 0.6        # m/s
+
+
