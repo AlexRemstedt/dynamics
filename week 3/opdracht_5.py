8d75f15f42cea27b454d479f01546194ba498eef
AlexRemstedt <49374085+AlexRemstedt@users.noreply.github.com>
2021-03-01 20:15:01 +0100

    Introduction of opdracht_5.py

---
+++ week 3/opdracht_5.py
@@ -0,0 +1,12 @@
+"""
+Alex Remstedt
+01/03/2020
+
+Opdracht 5 Week 4 - Energie
+https://secure.ans-delft.nl/universities/1/courses/63457/assignments/200359/quiz/show/38474119
+"""
+
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+
