144e4aa662ec3873a359e4bb0708da5762636a55
AlexRemstedt <49374085+AlexRemstedt@users.noreply.github.com>
2021-04-13 22:12:11 +0200

    fix(initial values): replace initial values for linear motions with vectors instead of integers

--- week 6/opdracht_2.py
+++ week 6/opdracht_2.py
@@ -26,8 +26,8 @@ time = np.linspace(t0, t1, round(1 + (t1 - t0) / dt))  # s
 # initial values
 theta_0 = 0
 omega_0 = 0
-x0 = 0
-v0 = 0
+x0 = np.array([0, 0])
+v0 = np.array([0, 0])
 
 
 # functions
@@ -61,6 +61,29 @@ def ang_kinematics(state, t):
     return omega, alpha
 
 
+def lin_kinematics(theta):
+    """
+    calculate the linear kinematics
+
+    :param theta: angles in radians
+
+    :type theta: np.ndarray
+
+    :return: tuple of position and velocity vectors in x and y direction.
+    :rtype: tuple
+    """
+    v = []
+    s = []
+
+    v.append(v0)
+    s.append(x0)
+    for n in range(len(theta) - 1):
+        a = np.array([force[0] * np.cos(theta[n]) / m, force[0] * np.sin(theta[n]) / m])
+        v.append(v[-1] + a * dt)
+        s.append(s[-1] + v[-2] * dt)
+    return s, v
+
+
 ang_kin = odeint(ang_kinematics, [theta_0, omega_0], time)
 
 theta_vec = ang_kin[:, 0]
@@ -68,35 +91,28 @@ omega_vec = ang_kin[:, 1]
 
 
 def main(pplot=False):
+    # answers
     a_ans = moment(force, radius)
     b_ans = moment(force, radius) / i
     [c_ans, d_ans] = from_accent(force / m, deg_to_rad(41))
+    [e_ans, f_ans] = lin_kinematics(theta_vec)[1][0]
     g_ans = theta_vec[-1]
+    [h_ans, i_ans] = lin_kinematics(theta_vec)[1][-1]
     j_ans = omega_vec[-1]
 
-    # E
-
-    if pplot:
-        return e_ans, f_ans
-    else:
-        # print
-        print(f"""
-        a) {a_ans}
-        b) {b_ans}
-        c) {c_ans}
-        d) {d_ans}
-        e) {e_ans}
-        f) {f_ans}
-        g) {g_ans}
-        j) {j_ans}
-        """)
-
-
-def plot():
-    e, f = main(pplot=True)
-    plt.plot(time, e)
-    plt.plot(time, f)
-    plt.show()
+    # print
+    print(f"""
+    a) {a_ans}
+    b) {b_ans}
+    c) {c_ans}
+    d) {d_ans}
+    e) {e_ans}
+    f) {f_ans}
+    g) {g_ans}
+    h) {h_ans}
+    i) {i_ans}
+    j) {j_ans}
+    """)
 
 
 # answers
