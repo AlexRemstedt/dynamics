6936e2340d49fdf0c692dae145ca30cf8583724f
AlexRemstedt <alex@remstedt.com>
2021-06-24 12:23:13 +0200

    add force functions

--- oefenen_her/ingewikkeld_veeren_systeem.py
+++ oefenen_her/ingewikkeld_veeren_systeem.py
@@ -12,3 +12,22 @@ obj 1
 :ans:
 """
 # imports
+import numpy as np
+
+def gravity(m, **kwargs):
+    if kwargs['vector']:
+        return np.array([0, -m * 9.81, 0])
+    return -m * 9.81
+
+def spring_force():
+    return None
+
+def _main():
+    return f"""
+{gravity(10, vector=True)=}
+
+"""
+
+
+if __name__ == "__main__":
+    print(_main())
