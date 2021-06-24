42bcfb3db87d6be37cd28778b3e6d5b0719f5962
AlexRemstedt <alex@remstedt.com>
2021-06-24 13:22:22 +0200

    add object vars

--- oefenen_her/ingewikkeld_veeren_systeem.py
+++ oefenen_her/ingewikkeld_veeren_systeem.py
@@ -13,14 +13,34 @@ obj 1
 """
 # imports
 import numpy as np
+from numpy.linalg import norm
 
-def gravity(m, **kwargs):
+# object (= circular)
+mass = 7  # kg
+r = np.array([1, 2, 0])
+centre = np.array([7, -7, 0])
+
+
+# spring 1 (BC)
+k = 6  # N/m
+l1 = np.array([2, 2, 0])  # m
+
+
+def gravity(m, **kwargs) -> np.ndarray[float]:
+    """Calculate the gravitational force of an object.
+    
+    :param float m: the mass of the object in kg
+    :return: gravitational force
+    """
     if kwargs['vector']:
         return np.array([0, -m * 9.81, 0])
     return -m * 9.81
 
-def spring_force():
-    return None
+
+def spring_force(u):
+    f = k * u
+    return f
+
 
 def _main():
     return f"""
